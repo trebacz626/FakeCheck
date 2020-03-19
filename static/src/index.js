4f577f4dad789de5eb4b7ff524b46ec6f66476be
Kacper Trebacz <trebacz.kacper@gmail.com>
2020-03-19 22:22:33 +0100

    vue fix

--- static/src/index.js
+++ static/src/index.js
@@ -2,14 +2,6 @@ import Vue from 'vue';
 import {store} from './store';
 import BootstrapVue from 'bootstrap-vue';
 
-Vue.component('account-balance-chart', require('./vue_components/AccountBalanceChart.vue').default);
-Vue.component('account-select', require('./vue_components/AccountSelect.vue').default);
-Vue.component('new-account', require('./vue_components/NewAccount.vue').default);
-Vue.component('new-transaction', require('./vue_components/NewTransaction.vue').default);
-Vue.component('account-balance-table', require('./vue_components/AccountBalanceTable.vue').default);
-Vue.component('new-category', require('./vue_components/NewCategory.vue').default);
-Vue.component('choice', require('./vue_components/Choice.vue').default);
-
 Vue.use(BootstrapVue);
 new Vue({
     el: "#app",
