61d52db1afd3a37e01404ba5d760067a89792716
Filip 'Cherit' Szóstak <31842263+Cheriit@users.noreply.github.com>
2020-03-20 22:34:15 +0100

    Update urls.py

--- FakeCheck/urls.py
+++ FakeCheck/urls.py
@@ -19,6 +19,6 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html'), name='index'),
-    path('fakechecker/', include('fakechecker.urls')),
+    path('', include('fakechecker.urls')),
     path('admin/', admin.site.urls),
 ]
