a5aa2ce3ddf0785944751df38c7835643bdf6314
MichalSz99 <m.szczepaniak99@gmail.com>
2020-03-20 13:32:02 +0100

    hotfix

--- FakeCheck/wsgi.py
+++ FakeCheck/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FakeChecker.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FakeCheck.settings')
 
 application = get_wsgi_application()
