ae24db23d885d469be0f32fd9ab25a42f00c7b18
Michał Szczepaniak <49671638+Szczepaniak-M@users.noreply.github.com>
2020-03-21 21:27:28 +0100

    index.html (#87)
    * Finalny?
    
    * index.html
    
    * index.html
    
    Co-authored-by: PatrykGrzegorek <patryk.grzegorek@o2.pl>
    Co-authored-by: PatrykGrzegorek <51320419+PatrykGrzegorek@users.noreply.github.com>

--- FakeCheck/urls.py
+++ FakeCheck/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 
+from fakechecker.views import QuestionForExpertListView
+
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html'), name='index'),
+    path('', QuestionForExpertListView.as_view(template_name='index.html'), name='index'),
     path('', include('fakechecker.urls')),
     path('admin/', admin.site.urls),
 ]
