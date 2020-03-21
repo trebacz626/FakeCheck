2684c5f095b0a8b6767004eebe782161852cd3ec
Filip 'Cherit' Szóstak <filip-szostak@wp.pl>
2020-03-21 22:53:50 +0100

    Fixes

--- static/src/collection_active_search.js
+++ static/src/collection_active_search.js
@@ -1,26 +1,30 @@
+import $ from "jquery";
+
+import { addToCollection, removeFromCollection, createCollectionRequest } from './requests';
+
 $(function () {
     $(".q-collection").on("changed.bs.select", function(e, clickedIndex, newValue, oldValue) {
-      var id = $(this).find('option').eq(clickedIndex).data('id');
-      var question_id = $(this).find('option').eq(clickedIndex).data('questionid');
+      const id = $(this).find('option').eq(clickedIndex).data('id');
+      const question_id = $(this).find('option').eq(clickedIndex).data('questionid');
       if (newValue){
         addToCollection(id,question_id)
       }else{
         removeFromCollection(id,question_id)
       }
     });
-  });
+});
 
 $('.new-collection').click( async function(){
-    name = prompt("Nazwa Kolekcji")
-    var collection_id = await createCollectionRequest(name)
+    const name = prompt("Nazwa Kolekcji");
+    const collection_id = await createCollectionRequest(name);
     if (! collection_id){
         return
     }
     $('.new-collection').each(function(){
-        var questionId = $(this).data('questionid')
-        var select = $(this).parent().find('select.q-collection').eq(0)
+        const questionId = $(this).data('questionid');
+        const select = $(this).parent().find('select.q-collection').eq(0);
         select.append(`<option data-id="${collection_id}" data-questionid="${questionId}">${name}</option>`);
         select.selectpicker("refresh")
     })
+});
 
-})
---
+++ static/src/requests.js
@@ -0,0 +1,40 @@
+import $ from "jquery";
+
+$.ajaxSetup({
+    beforeSend:function(xhr,settings){
+        xhr.setRequestHeader("X-CSRFToken",csrf)
+    }
+});
+
+const collectionManagementURL = (collection_id,question_id) => `http://${window.location.host}/collection/${collection_id}/question/${question_id}/`;
+const createCollectionURL = `http://${window.location.host}/collection/create/`;
+
+export async function addToCollection(collection_id,question_id){
+    try{
+    const result =await $.post(collectionManagementURL(collection_id,question_id))
+    }catch(err){
+        alert("Couldn't add to collection");
+    }
+}
+
+export async function removeFromCollection(collection_id,question_id){
+    try{
+    const result = await $.ajax({
+        url: collectionManagementURL(collection_id,question_id),
+        type: 'DELETE'
+    });
+
+    }catch(err){
+        alert("Couldn't remove from collection");
+    }
+}
+
+export async function createCollectionRequest(name){
+    try{
+    const result =await $.post(createCollectionURL,{'name':name});
+    return result.collection_id
+    }catch(err){
+        alert("Couldn't create collection");
+        return null
+    }
+}
