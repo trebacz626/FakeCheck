8a03d95d19b0e733b07d2fa8f4d296ab08731c12
Kacper Trebacz <kacper.trebacz@bluebrick.pl>
2020-03-22 01:00:17 +0100

    mixins
    fixes

--- fakechecker/questionCollectionViews.py
+++ fakechecker/questionCollectionViews.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 from django.views import generic, View
@@ -11,13 +12,20 @@ from .security import IsRedactorMixin, IsRedactorQuestionsAuthorMixin, IsNumberO
 from django.forms.models import model_to_dict
 
 
-class QuestionCollectionListView(generic.ListView):
+class QuestionCollectionListView(LoginRequiredMixin,
+                                IsRedactorMixin,
+                                generic.ListView):
     model = models.QuestionCollection
     form_class = forms.QuestionCollectionForm
     template_name = 'fakechecker/question_collection_list.html'
 
+    def get_queryset(self):
+        return models.QuestionCollection.objects.filter(redactor=self.request.user.redactor)
 
-class QuestionCollectionCreateView(IsRedactorMixin, View):
+
+class QuestionCollectionCreateView(LoginRequiredMixin,
+                                   IsRedactorMixin,
+                                   View):
     model = models.QuestionCollection
     form_class = forms.QuestionCollectionForm
 
@@ -32,12 +40,17 @@ class QuestionCollectionCreateView(IsRedactorMixin, View):
             return JsonResponse({'error':'please provide valid qyestion collection name'},status=500)
 
 
-class QuestionCollectionDetailView(generic.DetailView):
+
+class QuestionCollectionDetailView(LoginRequiredMixin,
+                                   IsRedactorMixin,
+                                   generic.DetailView):
     model = models.QuestionCollection
     form_class = forms.QuestionCollectionForm
     template_name = 'fakechecker/question_collection_detail.html'
 
-class QuestionCollectionViewQuestion(IsRedactorMixin, IsRedactorQuestionCollectionAuthorJSON,View):
+class QuestionCollectionViewQuestion(IsRedactorMixin,
+                                     IsRedactorQuestionCollectionAuthorJSON,
+                                     View):
 
     def get_data_from_url(self,request,collection_id,question_id):
         self.question_collection = get_object_or_404(models.QuestionCollection,id=collection_id)
